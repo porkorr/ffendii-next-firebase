f10b7b0fd0a700d7aed09be2496eb9205c823f8d
porkorr <78332643+porkorr@users.noreply.github.com>
2024-10-08 06:00:31 +0700

    Add files via upload

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,20 @@
 import AppProvider from "@/providers/AppProvider";
 import "@/styles/globals.css";
 
+// Metadata configuration
 export const metadata = {
   title: "ffendii",
   description: "request song",
-  // themeColor: "black",
-  // appleMobileWebAppStatusBarStyle: "black",
-  // viewport: {
-  //   width: "device-width",
-  //   initialScale: 1,
-  //   maximumScale: 1,
-  //   userScalable: false,
-  // },
+  themeColor: "black",
+  appleMobileWebAppStatusBarStyle: "black",
+};
+
+// Viewport configuration
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 const RootLayout = ({ children }) => {
